7c7823f138a7f88c58fee1dd9d607daa1cbf14fc
MarQuerz <marco.querzola@griffithuni.edu.au>
2024-09-04 09:54:50 +1000

    added flags ffor user online status

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ const userRoute = require('./routes/userRoute');
 
 app.use('/', userRoute);
 
+const io = require('socket.io')(http);
+
+var usp = io.of('/user-namespace');
+
+usp.on('connection', function(socket){
+    console.log('User Connected');
+
+    socket.on('disconnect', function(){
+        console.log('User Disconnected');
+    });
+});
+
 http.listen(3000, function(){
     console.log('Server is running');
 });

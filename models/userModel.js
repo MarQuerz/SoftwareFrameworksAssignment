46711920f41480d0d465f6045bc13da893b6c8f0
MarQuerz <marco.querzola@griffithuni.edu.au>
2024-09-04 06:21:18 +1000

    Created user and chat models

---
+++ models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        require:true
+    },
+    image:{
+        type:String,
+        require:true
+    },
+    password:{
+        type:String,
+        require:true
+    },
+    is_online:{
+        type:String,
+        default:'0'
+    }
+},
+{ timestamps:true }
+);
+
+module.exports = mongoose.Model('User', userSchema);

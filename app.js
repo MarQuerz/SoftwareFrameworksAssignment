889d27e5c5c9a01bd18bb73eb2d350d040563e48
MarQuerz <marco.querzola@griffithuni.edu.au>
2024-09-04 21:34:49 +1000

    added ability to load old chats

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const http = require('http').Server(app);
 
 const userRoute = require('./routes/userRoute');
 const User = require('./models/userModel');
+const Chat = require('./models/chatModel');
 
 app.use('/', userRoute);
 
@@ -38,6 +39,15 @@ usp.on('connection', async function(socket){
 
     socket.on('newChat', function(data){
         socket.broadcast.emit('loadNewChat', data);
+    });
+
+    socket.on('existsChat', async function(data){
+        var chats = await Chat.find({ $or:[
+            { sender_id: data.sender_id, receiver_id: data.receiver_id },
+            { sender_id: data.receiver_id, receiver_id: data.sender_id },
+        ]});
+
+        socket.emit('loadChats', { chats: chats });
     });
 });
 

ce4001f925bc440b79d4aac84ca12ff3183a6a04
MarQuerz <marco.querzola@griffithuni.edu.au>
2024-09-04 10:55:31 +1000

    dynamically change online status

--- app.js
+++ app.js
@@ -24,12 +24,16 @@ usp.on('connection', async function(socket){
 
     await User.findByIdAndUpdate({ _id: userId }, { $set:{ is_online:'1' } });
 
+    socket.broadcast.emit('getOnlineUser', { user_id: userId });
+
     socket.on('disconnect', async function(){
         console.log('User Disconnected');
 
         var userId = socket.handshake.auth.token;
 
         await User.findByIdAndUpdate({ _id: userId }, { $set:{ is_online:'0' } });
+
+        socket.broadcast.emit('getOfflineUser', { user_id: userId });
     });
 });
 

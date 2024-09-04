17f9be64c8dc7c929b6e51803cb03178496c4592
MarQuerz <marco.querzola@griffithuni.edu.au>
2024-09-04 22:08:56 +1000

    started implementing delete message

--- controllers/userController.js
+++ controllers/userController.js
@@ -107,6 +107,16 @@ const saveChat = async(req, res)=>{
     }
 }
 
+const deleteChat = async(req, res)=>{
+    try {
+        await Chat.deleteOne({ _id: req.body.id })
+
+        res.status(200).send({ success: true });
+    } catch (error) {
+        res.status(400).send({ success: false, msg: error.message });
+    }
+}
+
 module.exports = {
     registerLoad,
     register,
@@ -114,5 +124,6 @@ module.exports = {
     login,
     logout,
     loadDashboard,
-    saveChat
+    saveChat,
+    deleteChat
 }
